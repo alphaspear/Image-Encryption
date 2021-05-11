a3ba7c0a0b6035353d9315839266d497651e9f88
Abhilash Joshi <iabhilashjoshi@gmail.com>
2021-05-11 17:52:16 +0530

    removed a file containing functions

--- decrypt.py
+++ decrypt.py
@@ -6,7 +6,31 @@ except ImportError:
 from random import randint
 import numpy
 import sys
-from helper import *
+
+def upshift(a,index,n):
+    col = []
+    for j in range(len(a)):
+        col.append(a[j][index])
+    shiftCol = numpy.roll(col,-n)
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            if(j==index):
+                a[i][j] = shiftCol[i]
+
+def downshift(a,index,n):
+    col = []
+    for j in range(len(a)):
+        col.append(a[j][index])
+    shiftCol = numpy.roll(col,n)
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            if(j==index):
+                a[i][j] = shiftCol[i]
+
+def rotate180(n):
+    bits = "{0:b}".format(n)
+    return int(bits[::-1], 2)
+
 
 im = Image.open(sys.argv[1])
 pix = im.load()
